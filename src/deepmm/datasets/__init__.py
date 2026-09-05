6c346e9f9f05ee430b24feea0834b7c1d8f5a946
EkodeckStephane <166718028+EkodeckStephane@users.noreply.github.com>
2026-09-05 19:08:53 +0100

    Add dataset adapters package

---
+++ src/deepmm/datasets/__init__.py
@@ -0,0 +1,11 @@
+from .nupt_fpv import (
+    NUPT_PUBLIC_UNRESOLVED_PERSON,
+    assert_nupt_person_mapping_resolved,
+    scan_nupt_fpv,
+)
+
+__all__ = [
+    "NUPT_PUBLIC_UNRESOLVED_PERSON",
+    "scan_nupt_fpv",
+    "assert_nupt_person_mapping_resolved",
+]
