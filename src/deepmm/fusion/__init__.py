24e91f1f2d5b693d297dcc25e83ab08b33c68e36
EkodeckStephane <166718028+EkodeckStephane@users.noreply.github.com>
2026-09-05 16:11:20 +0100

    Add fusion package

---
+++ src/deepmm/fusion/__init__.py
@@ -0,0 +1,8 @@
+from .classical import LogisticScoreFusion, WeightedScoreFusion, zscore_fit, zscore_transform
+
+__all__ = [
+    "WeightedScoreFusion",
+    "LogisticScoreFusion",
+    "zscore_fit",
+    "zscore_transform",
+]
